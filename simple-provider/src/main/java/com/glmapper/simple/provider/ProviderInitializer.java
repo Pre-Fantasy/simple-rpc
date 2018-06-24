8d0e92ee3a6a73bc9da3529d24e24259b0d62799
Pre-Fantasy <407673434@qq.com>
2018-06-24 17:59:23 +0800

    初始化服务 1首先他实现了ApplicationContextAware，InitializingBean 这两个spring接口，根据IOC容器初始化的顺序，会依次调用接口中的setApplicationContext 和afterPcationContext方法中获取了容器被@SimpleProvider标注的类，并将服务接口名和服务实现类绑定，存放到HandlerMap中，在@SimpleProvider中有一个value属性，是考虑到一个类可以实现多个接口，通过value可以指定那个服务接口，当然也可以定义为数组，处理多个接口。 afterPropertiesSet方法中做了两件事： 	1.在服务端开启了一个待处理socket请求的线程池，蒋婷和处理服务暴露端口上接收到的请求，制定了一个处理SimpleHandler 	2.调用ServiceRegisty类的registry方法想zookeeper注册服务的地址和端口，这个没有用到协议，只注册了ip：port setApplicationContext

---
+++ simple-provider/src/main/java/com/glmapper/simple/provider/ProviderInitializer.java
@@ -0,0 +1,163 @@
+package com.glmapper.simple.provider;/**
+ * @author Pre_fantasy
+ * @create 2018-06-24 16:37
+ * @desc
+ **/
+
+import com.glmapper.simple.properties.SimpleProviderProperties;
+import com.glmapper.simple.provider.annotation.SimpleProvider;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.beans.AppletInitializer;
+import java.net.Inet4Address;
+import java.net.NetworkInterface;
+import java.nio.channels.SocketChannel;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.logging.SocketHandler;
+
+/**
+
+ * @author dell
+
+ * @create 2018-06-24 16:37
+
+ * @desc 启动并注册服务
+
+ **/
+public class ProviderInitializer implements ApplicationContextAware, InitializingBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProviderInitializer.class);
+
+    private SimpleProviderProperties providerProperties;
+
+    /*service regist;*/
+    private ServiceRegistry serviceRegistry;
+
+
+    private Map<String, Object> handlerMap = new HashMap<String, Object>();
+
+    public ProviderInitializer(SimpleProviderProperties providerProperties, ServiceRegistry serviceRegistry) {
+            this.providerProperties = providerProperties;
+            this.serviceRegistry = serviceRegistry;
+    }
+
+    public void setApplicationContext(ApplicationContext crx) throws Exception {
+
+        /*获取被SimpleProvider注解的Bean*/
+        Map<String, Object> serviceBeanMap = ctx.getB.keansWithAnnotation(SimpleProvider.class);
+        if (MapUtils.isNotEmpty(serviceBeanMap)) {
+            for (Object serviceBean : serviceBeanMap.values()) {
+                String interfaceName = serviceBean.getClass().getAnnotation(SimpleProvider.class).value().getName();
+                handlerMap.put(interfaceName, serviceBean);
+            }
+        }
+    }
+
+    public void afterPropertiesSet() throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            ChannelHandler channelHandler = new ChannelInitializer<SocketChannel>{
+                public void initChannel(SocketChannel channel) throws Exception{
+                    channel.pipeline()
+                            .addLast(new SimpleDecoder(SimpleRequest.class))
+                            .addLast(new SimpleEncoder(SimpleResponse.class))
+                            .addLast(new SimpleHandler(handlerMap));
+                }
+            };
+            bootstrap.group(bossGroup, wordGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(channelHandler)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            String host = getLocalHost();
+            if (host == null) {
+                LOGGER.error("can't get service address,because address is null");
+                throw new SimpleException("can't get service address,because address is null");
+            }
+
+            int port = providerProperties.getProt();
+            ChannelFuter future = bootstrap.bind(host, port).sync();
+            LOGGER.debug("server started on port { }", port);
+            if (serviceRegistry != null) {
+                String serverAddress = host + ":" + port;
+                serviceRegistry.register(serverAddress);
+            }
+            future.channel().closeFuture().sync();
+
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+
+    }
+
+    private String getLocalHost (){
+
+        Enumeration<NetworkInterface> allNetInterface;
+        try {
+            allNetInterface = NetworkInterface.getNetworkInterfaces();
+        } catch (Exception e) {
+            LOGGER.error("get local address error , cause: ", e);
+            return null;
+        }
+        while (allNetInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = allNetInterface.nextElement();
+            Enumeration<InetAddress> addresses = netIterface.getInetAddress();
+            while (addresses.hasMoreElements()) {
+                InetAddress ip = addresses.nextElement();
+                if (ip instanceof Inet4Address && !ip.isLoopbackAddress() && !ip.getHostAddress().contains(":")) {
+                    return ip.getHostAddress();
+                }
+            }
+        }
+        return null;    
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
