92cd668e0f37875417b18e999315c71d9bb6beef
Pre-Fantasy <407673434@qq.com>
2018-06-24 14:24:24 +0800

    Ingitial project

---
+++ simple-api/src/main/java/com/glmapper/simpleapi/SimpleApiApplication.java
@@ -0,0 +1,12 @@
+package com.glmapper.simpleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApiApplication.class, args);
+    }
+}
